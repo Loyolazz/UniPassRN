fc34a062c2b0c103ef0f4d38657682b7b018e557
VictorEmanuell <victoremanuelexx@gmail.com>
2023-06-14 22:50:35 -0300

    building workload

--- src/screens/Student/Workload/index.jsx
+++ src/screens/Student/Workload/index.jsx
@@ -1,8 +1,75 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import CircularProgress from 'react-native-circular-progress-indicator';
+
+import IconAddCourse from '../../../assets/Icons/icon-add-course.png';
 
 import { styles } from './styles';
 
-export function Workload() {
-	return <View style={styles.container}></View>;
+import { Header } from '../../../components/Header';
+import Fonts from '../../../assets/Fonts';
+
+export function Workload({ navigation }) {
+	return (
+		<View style={styles.container}>
+			<Header navigation={navigation} title="Carga Horária" />
+
+			<View style={styles.containerProgress}>
+				<View style={styles.boxProgress}>
+					<CircularProgress
+						value={40}
+						maxValue={120}
+						progressValueStyle={{ fontFamily: Fonts.bold, fontSize: 20, color: 'white' }}
+						title="HORAS"
+						titleStyle={{ fontFamily: Fonts.bold, fontSize: 20, color: 'white' }}
+						valueSuffix="/120"
+						valueSuffixStyle={{ fontFamily: Fonts.bold, fontSize: 20, color: 'white' }}
+						activeStrokeColor="#52FF00"
+						inActiveStrokeColor="#FF0000"
+						inActiveStrokeWidth={6}
+						radius={80}
+						duration={1000}
+					/>
+				</View>
+
+				<View style={styles.containerCaptions}>
+					<View style={styles.boxCaptions}>
+						<View
+							style={{
+								aspectRatio: 1,
+								width: 20,
+								backgroundColor: '#52FF00',
+								borderRadius: 100,
+							}}
+						/>
+
+						<Text style={styles.textCaptions}>Concluído</Text>
+					</View>
+
+					<View style={styles.boxCaptions}>
+						<Text style={styles.textCaptions}>A concluir</Text>
+
+						<View
+							style={{
+								aspectRatio: 1,
+								width: 20,
+								backgroundColor: '#FF0000',
+								borderRadius: 100,
+							}}
+						/>
+					</View>
+				</View>
+			</View>
+
+			<View style={styles.containerAddCourse}>
+				<Text style={styles.textAddCourse}>Cursos / Atividades</Text>
+
+				<TouchableOpacity activeOpacity={0.8} style={styles.boxIconAddCourse}>
+					<Image source={IconAddCourse} style={styles.iconAddCourse} resizeMode="center" />
+				</TouchableOpacity>
+			</View>
+
+			<View style={styles.containerCertificates}></View>
+		</View>
+	);
 }
--- src/screens/Student/Workload/styles.js
+++ src/screens/Student/Workload/styles.js
@@ -4,5 +4,69 @@ import Colors from '../../../assets/Colors';
 import Fonts from '../../../assets/Fonts';
 
 export const styles = StyleSheet.create({
-	container: {},
+	container: {
+		flex: 1,
+		backgroundColor: 'white',
+		alignItems: 'center',
+	},
+	containerProgress: {
+		backgroundColor: Colors.bluePrimary,
+		width: '100%',
+		minHeight: '28%',
+		borderBottomLeftRadius: 30,
+		borderBottomRightRadius: 30,
+		alignItems: 'center',
+	},
+	boxProgress: {
+		borderRadius: 100,
+		borderWidth: 1,
+		borderColor: 'white',
+		padding: 3.5,
+	},
+	containerCaptions: {
+		position: 'absolute',
+		bottom: 0,
+		paddingVertical: 20,
+		width: '90%',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+	},
+	boxCaptions: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 6
+	},
+	textCaptions: {
+		fontFamily: Fonts.regular,
+		fontSize: 15,
+		color: 'white',
+	},
+	containerAddCourse: {
+		width: '90%',
+		paddingVertical: 20,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+	},
+	textAddCourse: {
+		fontFamily: Fonts.bold,
+		fontSize: 25,
+		color: 'black',
+	},
+	boxIconAddCourse: {
+		aspectRatio: 1,
+		width: 40,
+	},
+	iconAddCourse: {
+		width: '100%',
+		height: '100%',
+	},
+	containerCertificates: {
+		width: '100%',
+		height: '100%',
+		backgroundColor: Colors.gray,
+		borderTopLeftRadius: 30,
+		borderTopRightRadius: 30,
+	},
 });
